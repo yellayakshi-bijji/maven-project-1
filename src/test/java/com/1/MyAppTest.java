0970f197ea429fa716b82cbd136c89a8f6e8cd49
Yellayakshi Bijji <yellayakshi.bijji.135@gmail.com>
2023-10-03 20:21:52 -0500

    modified MyAppTest.java

--- src/test/java/com/1/MyAppTest.java
+++ src/test/java/com/1/MyAppTest.java
@@ -3,9 +3,9 @@ package com.1;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class MyAppTest {
+public class MyAppTest{
     @Test
-    public void testMyApp() {
+    public void testMyApp(){
         String message = "Hello, Maven!";
         assertTrue(message.equals("Hello, Maven!"));
     }
