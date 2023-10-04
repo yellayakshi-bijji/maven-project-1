d69f258c7bf6ce490d6a4a9fa7654945c162fe2e
Yellayakshi Bijji <yellayakshi.bijji.135@gmail.com>
2023-10-03 20:11:56 -0500

    maven-project-1

---
+++ src/test/java/com/1/MyAppTest.java
@@ -0,0 +1,12 @@
+package com.1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyAppTest {
+    @Test
+    public void testMyApp() {
+        String message = "Hello, Maven!";
+        assertTrue(message.equals("Hello, Maven!"));
+    }
+}
